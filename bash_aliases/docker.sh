e308fefb687c521b6d9f0b4e7f5f96e7d8134114
Miles Stanfield <milesstanfield@gmail.com>
2023-11-03 15:08:43 -0400

    moved docker

--- bash_aliases/docker.sh
+++ bash_aliases/docker.sh
@@ -4,11 +4,11 @@ alias dcprune="docker system prune"
 alias dcpruneall="docker system prune -a"
 
 dcstart_fun() {
-  docker compose --file ~/code/setup/docker-compose.yml up $1 --detach
+  docker compose --file ~/code/setup/docker/docker-compose.yml up $1 --detach
 }
-alias dcstart=dcstart_fun
+alias dcstart=dcstart_fun # dcstart postgres-shared
 
 dc_fun() {
-  docker compose --file ~/code/setup/docker-compose.yml $@
+  docker compose --file ~/code/setup/docker/docker-compose.yml $@
 }
 alias dc=dc_fun
