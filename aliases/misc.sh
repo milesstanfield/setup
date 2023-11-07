1ae6a5851f25c974665d10ae32f06b01208a5f84
Miles Stanfield <milesstanfield@gmail.com>
2023-11-07 15:43:33 -0500

    minor tweaks

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -7,6 +7,7 @@ alias bootbios="sudo systemctl reboot --firmware-setup"
 # interactive mechanism to track which keys the system recognizes was pressed.
 # useful if using different types of keyboards to see what the system thinks the keys are
 track_key() {
+  warn "can also run 'sudo showkey'"
   xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'
 }
 
@@ -25,6 +26,8 @@ alias stopclamd="sudo systemctl stop clamav-daemon.service"
 freshclamscan() {
   warn "note: this was run without sudo. if you need to scan files only root has"
   warn "access to, you will need to prepend sudo. example: 'sudo clamdscan --fdpass /usr'"
+  # todo i should save the timestamp of the scan and then create warnings in bashrc script
+  # if i havent scanned in a long time
   clamdscan --fdpass /
 }
 
