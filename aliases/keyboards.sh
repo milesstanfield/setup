d95fd5a2911a7447becb23c8be193033c4319ac5
Miles Stanfield <milesstanfield@gmail.com>
2023-11-08 08:42:19 -0500

    added some comments for visualizing the keyboard changes

--- aliases/keyboards.sh
+++ aliases/keyboards.sh
@@ -2,19 +2,19 @@
 
 alias reset_keys="setxkbmap -layout us"
 alias mac="reset_keys && xmodmap ~/code/setup/xmodmaps/super_L_as_ctrl_L.Xmodmap"
-# ----NOW
-# FN
-# Control_L
-# Alt_L
-# Control_L
+# ----WAS           ----NOW
+# FN                FN
+# Control_L         Control_L
+# Alt_L             Alt_L
+# Super_L           Control_L
 
 win() {
   reset_keys
   xmodmap ~/code/setup/xmodmaps/switch_alt_L_and_super_L.Xmodmap
   xmodmap ~/code/setup/xmodmaps/super_L_as_ctrl_L.Xmodmap
 }
-# ----NOW
-# Control_L
-# FN
-# Alt_L
-# Control_L
+# ----WAS           -----NOW
+# FN                Control_L
+# Control_L         FN
+# Super_L           Alt_L
+# Alt_L             Control_L
