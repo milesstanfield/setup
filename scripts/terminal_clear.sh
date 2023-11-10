85da5aec7ba584ef6c3133b1ae056f7799e042fe
Miles Stanfield <milesstanfield@gmail.com>
2023-11-09 20:35:48 -0500

    fixed terminal --clear

--- scripts/terminal_clear.sh
+++ scripts/terminal_clear.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+# https://manpages.ubuntu.com/manpages/focal/man1/xdotool.1.html
 # usage: bash ~/code/setup/scripts/terminal_clear.sh
 
-clear
-exit 0
+sleep 0.06
+xdotool key shift+ctrl+k ctrl+l
