bd39ef8a50b73ff486ba7653a7d53ffa3514a6d7
Miles Stanfield <milesstanfield@gmail.com>
2023-11-17 23:22:03 -0500

    added tmp binme alias

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -4,3 +4,7 @@ alias aliases="alias -p"
 alias boot_bios="sudo systemctl reboot --firmware-setup"
 alias pidstatus="warn $$ && cat /proc/$$/status"
 # todo curl how-to
+
+binme() { # todo take this out when binme is done
+  cargo run "$@"
+}
