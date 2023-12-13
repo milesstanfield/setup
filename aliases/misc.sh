61a54b967a2fb1a364cbd88acd135894a230e675
Miles Stanfield <milesstanfield@gmail.com>
2023-12-13 11:02:56 -0500

    changed misc binme to finme

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -5,6 +5,6 @@ alias boot_bios="sudo systemctl reboot --firmware-setup"
 alias pidstatus="warn $$ && cat /proc/$$/status"
 # todo curl how-to
 
-binme() { # todo take this out when binme is done
+finme() {
   cargo run "$@"
 }
